8a2e0f8c686784356e65a1beebc035710b627f34
Tazkia Hassan Soha <tazkiasoha@gmail.com>
2018-10-04 03:16:04 +0000

    master

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* JavaScript code here */
+console.log("external");
+
+// Task 2
+
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index) { 
+        sum = sum + value;
+        
+    });
+    return sum;
+}
+console.log(sumArray([2, 4, 5, 6, 7]));
